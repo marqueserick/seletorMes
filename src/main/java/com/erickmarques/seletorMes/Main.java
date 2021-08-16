3518ce6c9d7f81d585e854c5ed403251780e3f8e
Erick Marques <erickmarques43@gmail.com>
2021-08-16 19:53:07 -0300

    first commit

---
+++ src/main/java/com/erickmarques/seletorMes/Main.java
@@ -0,0 +1,70 @@
+package com.erickmarques.seletorMes;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int mes = 1;
+		System.out.println("Digite um valor de 1 a 12 para selecionar um mês");
+		try {
+			mes = sc.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("Valor inserido é inválido\n" + "O mês 1 será exibido");
+			mes = 1;
+		}
+		System.out.println(exibirMes(mes));
+
+	}
+
+	public static String exibirMes(int mes) {
+		String mesSelecionado="Você selecionou o mês de ";
+		
+		switch (mes) {
+		case 1:
+			mesSelecionado+="Janeiro";
+			break;
+		case 2:
+			mesSelecionado+="Fevereiro";
+			break;
+		case 3:
+			mesSelecionado+="Março";
+			break;
+		case 4:
+			mesSelecionado+="Abril";
+			break;
+		case 5:
+			mesSelecionado+="Maio";
+			break;
+		case 6:
+			mesSelecionado+="Junho";
+			break;
+		case 7:
+			mesSelecionado+="Julho";
+			break;
+		case 8:
+			mesSelecionado+="Agosto";
+			break;
+		case 9:
+			mesSelecionado+="Setembro";
+			break;
+		case 10:
+			mesSelecionado+="Outubro";
+			break;
+		case 11:
+			mesSelecionado+="Novembro";
+			break;
+		case 12:
+			mesSelecionado+="Dezembro";
+			break;
+		default:
+			mesSelecionado="Valor selecionando não corresponde a um mês do ano";
+		}
+		
+		return mesSelecionado;
+	}
+
+}
