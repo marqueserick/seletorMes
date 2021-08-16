8730affd02765ba5962b9ff1823bc97cd0186936
Erick Marques <erickmarques43@gmail.com>
2021-08-16 20:16:23 -0300

    implementando test

---
+++ src/test/java/com/erickmarques/seletorMes/MainTest.java
@@ -0,0 +1,21 @@
+package com.erickmarques.seletorMes;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static com.erickmarques.seletorMes.Main.exibirMes;
+
+public class MainTest {
+	
+	@Test
+	void deveExibirMesJaneiro() {
+		assertEquals("Você selecionou o mês de Janeiro", exibirMes(1));
+		
+	}
+	
+	@Test 
+	void naoDeveExibirMes() {
+		assertEquals("Valor selecionando não corresponde a um mês do ano", exibirMes(13));
+	}
+
+}
